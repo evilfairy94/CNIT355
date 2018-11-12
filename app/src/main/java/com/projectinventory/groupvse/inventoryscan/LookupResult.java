146db02bb9ae60bec832e72c8a72b8a8d2b3bccb
Scott Jean <scott.jean@gmail.com>
2018-11-12 17:04:05 -0500

    All activities are here still need to add intents to the buttons.

---
+++ app/src/main/java/com/projectinventory/groupvse/inventoryscan/LookupResult.java
@@ -0,0 +1,13 @@
+package com.projectinventory.groupvse.inventoryscan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LookupResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lookup_result);
+    }
+}
---
+++ app/src/main/java/com/projectinventory/groupvse/inventoryscan/LookupStation.java
@@ -0,0 +1,13 @@
+package com.projectinventory.groupvse.inventoryscan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LookupStation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lookup_station);
+    }
+}
