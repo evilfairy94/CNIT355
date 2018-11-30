3e2c698d95af23d394ffc2ed0ce94b5039526e26
evilfairy94 <evazeintl@yahoo.de>
2018-11-30 15:58:46 -0500

    implemented service for exporting the Data into a file.

--- app/src/main/java/com/projectinventory/groupvse/inventoryscan/Home.java
+++ app/src/main/java/com/projectinventory/groupvse/inventoryscan/Home.java
@@ -1,13 +1,20 @@
 package com.projectinventory.groupvse.inventoryscan;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
 
-    //Vishrut added
+    InventoryDbHelper mDbHelper;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +33,27 @@ public class Home extends AppCompatActivity {
     }
 
     public void exportData(View view) {
-        //Start service
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        1);
+            }
+        }
+
+        Intent Service = new Intent(this, exporting.class);
+        try {
+            startService(Service);
+
+        } catch (Exception e) {
+            Toast.makeText(this, "File could not be saved", Toast.LENGTH_SHORT).show();
+        }
+
     }
+
+
 }
 
